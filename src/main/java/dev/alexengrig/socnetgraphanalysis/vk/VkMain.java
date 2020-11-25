e1d3e56fbe52c85e920ce968ec6e4376e617cec7
Grig Alex <alexengrigdev@gmail.com>
2020-11-26 01:26:54 +0300

    Refactor VkMain

--- src/main/java/dev/alexengrig/socnetgraphanalysis/vk/VkMain.java
+++ src/main/java/dev/alexengrig/socnetgraphanalysis/vk/VkMain.java
@@ -1,70 +1,63 @@
 package dev.alexengrig.socnetgraphanalysis.vk;
 
-import com.vk.api.sdk.client.VkApiClient;
-import com.vk.api.sdk.client.actors.ServiceActor;
-import com.vk.api.sdk.exceptions.ApiException;
-import com.vk.api.sdk.exceptions.ClientException;
-import com.vk.api.sdk.httpclient.HttpTransportClient;
 import com.vk.api.sdk.objects.users.UserXtrCounters;
-import com.vk.api.sdk.queries.friends.FriendsGetQuery;
-import com.vk.api.sdk.queries.users.UsersGetQuery;
 
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Queue;
-import java.util.Set;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Collectors;
 
 public class VkMain {
 
+    public static final String FIRST_USER_ID = "0";
     public static final int APP_ID = 0;
     public static final String APP_TOKEN = "0";
-    public static final ServiceActor ACTOR = new ServiceActor(APP_ID, APP_TOKEN);
-    public static final HttpTransportClient TRANSPORT_CLIENT = HttpTransportClient.getInstance();
-    public static final VkApiClient VK_CLIENT = new VkApiClient(TRANSPORT_CLIENT);
-    public static final UsersGetQuery GET_USER = new UsersGetQuery(VK_CLIENT, ACTOR);
-    public static final FriendsGetQuery GET_FRIENDS = new FriendsGetQuery(VK_CLIENT, ACTOR);
+    public static final VkService vk = new VkService(APP_ID, APP_TOKEN);
+    public static final ForkJoinPool pool = new ForkJoinPool(6);
+    public static final ExecutorService executor = Executors.newFixedThreadPool(4);
 
-    public static void main(String[] args) throws ClientException, ApiException {
-        UserXtrCounters firstUser = getUserById("0");
-        Set<Integer> allUsers = new HashSet<>(Collections.singleton(firstUser.getId()));
-        Queue<UserXtrCounters> queue = new ConcurrentLinkedQueue<>(Collections.singleton(firstUser));
-        while (!queue.isEmpty()) {
+    public static void main(String[] args) {
+        UserXtrCounters firstUser = vk.getUser(FIRST_USER_ID);
+        Integer firstUserId = firstUser.getId();
+        Map<Integer, Boolean> allUsers = new ConcurrentHashMap<>();
+        allUsers.put(firstUserId, true);
+        ConcurrentLinkedQueue<UserXtrCounters> queue = new ConcurrentLinkedQueue<>(Collections.singleton(firstUser));
+        while (true) {
             UserXtrCounters user = queue.poll();
-            List<String> friendIds = getFriendIdsByUserId(user.getId());
-            List<UserXtrCounters> friends = getUsersByIds(friendIds);
-            queue.addAll(friends.stream()
-                    .filter(u -> {
-                        if (allUsers.contains(u.getId())) {
-                            return false;
+            if (user != null) {
+                executor.execute(() -> {
+                    List<String> friendIds = vk.getFriendIds(user.getId());
+                    List<UserXtrCounters> friends = vk.getUsers(friendIds);
+                    if (!friends.isEmpty()) {
+                        try {
+                            queue.addAll(pool.submit(() -> friends.parallelStream()
+                                    .filter(u -> {
+                                        Integer userId = u.getId();
+                                        if (allUsers.containsKey(userId)) {
+                                            return false;
+                                        }
+                                        String url = "https://vk.com/id" + userId;
+                                        if (vk.getFollowerIds(userId).contains(firstUserId)) {
+                                            System.out.println("Follower: " + url);
+                                        }
+                                        allUsers.put(userId, true);
+                                        return true;
+                                    })
+                                    .collect(Collectors.toList()))
+                                    .get());
+                        } catch (InterruptedException | ExecutionException e) {
+                            e.printStackTrace();
                         }
-                        System.out.println("https://vk.com/id" + u.getId());
-                        return true;
-                    })
-                    .collect(Collectors.toList()));
-        }
-    }
-
-    private static UserXtrCounters getUserById(String id) throws ClientException, ApiException {
-        return GET_USER.userIds(id).execute().get(0);
-    }
-
-    private static List<String> getFriendIdsByUserId(Integer userId) {
-        try {
-            return GET_FRIENDS.userId(userId).execute()
-                    .getItems().stream().map(String::valueOf).collect(Collectors.toList());
-        } catch (ApiException | ClientException e) {
-            return Collections.emptyList();
-        }
-    }
-
-    private static List<UserXtrCounters> getUsersByIds(List<String> ids) {
-        try {
-            return GET_USER.userIds(ids).execute();
-        } catch (ApiException | ClientException e) {
-            return Collections.emptyList();
+                        System.out.println("Queue: " + queue.size() + "\tUsers: " + allUsers.size());
+                    }
+                });
+            }
         }
     }
 }
