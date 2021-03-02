1735668281349d6b6152ca146b8432fb84d83fed
Grig Alex <alexengrigdev@gmail.com>
2021-03-03 01:01:09 +0300

    Create SnapshotRepository

--- src/main/java/dev/alexengrig/socnetgraphanalysis/repository/SnapshotRepository.java
+++ src/main/java/dev/alexengrig/socnetgraphanalysis/repository/SnapshotRepository.java
@@ -1,9 +1,9 @@
 package dev.alexengrig.socnetgraphanalysis.repository;
 
-import dev.alexengrig.socnetgraphanalysis.entity.Person;
+import dev.alexengrig.socnetgraphanalysis.entity.Snapshot;
 import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonRepository extends ReactiveNeo4jRepository<Person, Long> {
+public interface SnapshotRepository extends ReactiveNeo4jRepository<Snapshot, Long> {
 }
