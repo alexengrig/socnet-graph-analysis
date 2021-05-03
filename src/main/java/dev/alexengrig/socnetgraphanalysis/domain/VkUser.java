860305df23d1cc8c0def82e2181b615331c1d080
Grig Alex <alexengrigdev@gmail.com>
2021-05-03 22:18:53 +0300

    Update VkUser

--- src/main/java/dev/alexengrig/socnetgraphanalysis/domain/VkUser.java
+++ src/main/java/dev/alexengrig/socnetgraphanalysis/domain/VkUser.java
@@ -10,10 +10,31 @@ public class VkUser {
     private String firstName;
     private String lastName;
     private String birthday;
-
     private Integer age;
 
     private boolean accessed;
 
+    private Integer sex;
+    private Integer city;
+    private Integer country;
+    private Integer friendStatus;
+
+    private Integer relation;
+    private Integer political;
+    private Integer religion;
+    private Integer lifeMain;
+    private Integer peopleMain;
+    private Integer smoking;
+    private Integer alcohol;
+
+    private Integer commonFriendsCount;
+    private Integer friendsCount;
+    private Integer followersCount;
     private Integer audiosCount;
+    private Integer videosCount;
+    private Integer photosCount;
+    private Integer groupsCount;
+    private Integer albumsCount;
+    private Integer notesCount;
+    private Integer pagesCount;
 }
