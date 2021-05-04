fd90c8a9a3cc303121f48b17b4c4e815c23c8d41
Grig Alex <alexengrigdev@gmail.com>
2021-05-04 23:32:55 +0300

    Update ClusterRecordConverter

--- src/main/java/dev/alexengrig/socnetgraphanalysis/converter/ClusterRecordConverter.java
+++ src/main/java/dev/alexengrig/socnetgraphanalysis/converter/ClusterRecordConverter.java
@@ -7,6 +7,8 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
 public class ClusterRecordConverter implements Converter<VkUser, ClusterRecord> {
 
@@ -16,31 +18,30 @@ public class ClusterRecordConverter implements Converter<VkUser, ClusterRecord> 
         return ClusterRecord.builder()
                 .id(source.getId())
                 .label(String.join(" ", source.getFirstName(), source.getLastName()))
-                .parameters(ClusterRecordParameters.builder()
-                        .parameter("Accessed", source.isAccessed() ? 1d : 0d)
-                        .parameter("Age", source.getAge().doubleValue())
-                        .parameter("Sex", source.getSex().doubleValue())
-                        .parameter("City", source.getCity().doubleValue())
-                        .parameter("Country", source.getCountry().doubleValue())
-                        .parameter("Friend status", source.getFriendStatus().doubleValue())
-                        .parameter("Relation", source.getRelation().doubleValue())
-                        .parameter("Political", source.getPolitical().doubleValue())
-                        .parameter("Religion", source.getReligion().doubleValue())
-                        .parameter("Life main", source.getLifeMain().doubleValue())
-                        .parameter("People main", source.getPeopleMain().doubleValue())
-                        .parameter("Smoking", source.getSmoking().doubleValue())
-                        .parameter("Alcohol", source.getAlcohol().doubleValue())
-                        .parameter("Common friends count", source.getCommonFriendsCount().doubleValue())
-                        .parameter("Friends count", source.getFriendsCount().doubleValue())
-                        .parameter("Followers count", source.getFollowersCount().doubleValue())
-                        .parameter("Audios count", source.getAudiosCount().doubleValue())
-                        .parameter("Videos count", source.getVideosCount().doubleValue())
-                        .parameter("Photos count", source.getPhotosCount().doubleValue())
-                        .parameter("Groups count", source.getGroupsCount().doubleValue())
-                        .parameter("Albums count", source.getAlbumsCount().doubleValue())
-                        .parameter("Notes count", source.getNotesCount().doubleValue())
-                        .parameter("Pages count", source.getPagesCount().doubleValue())
-                        .build())
+                .parameters(new ClusterRecordParameters(Map.ofEntries(
+                        Map.entry("Accessed", source.isAccessed() ? 1d : 0d),
+                        Map.entry("Age", source.getAge().doubleValue()),
+                        Map.entry("Sex", source.getSex().doubleValue()),
+                        Map.entry("City", source.getCity().doubleValue()),
+                        Map.entry("Country", source.getCountry().doubleValue()),
+                        Map.entry("Friend status", source.getFriendStatus().doubleValue()),
+                        Map.entry("Relation", source.getRelation().doubleValue()),
+                        Map.entry("Political", source.getPolitical().doubleValue()),
+                        Map.entry("Religion", source.getReligion().doubleValue()),
+                        Map.entry("Life main", source.getLifeMain().doubleValue()),
+                        Map.entry("People main", source.getPeopleMain().doubleValue()),
+                        Map.entry("Smoking", source.getSmoking().doubleValue()),
+                        Map.entry("Alcohol", source.getAlcohol().doubleValue()),
+                        Map.entry("Common friends count", source.getCommonFriendsCount().doubleValue()),
+                        Map.entry("Friends count", source.getFriendsCount().doubleValue()),
+                        Map.entry("Followers count", source.getFollowersCount().doubleValue()),
+                        Map.entry("Audios count", source.getAudiosCount().doubleValue()),
+                        Map.entry("Videos count", source.getVideosCount().doubleValue()),
+                        Map.entry("Photos count", source.getPhotosCount().doubleValue()),
+                        Map.entry("Groups count", source.getGroupsCount().doubleValue()),
+                        Map.entry("Albums count", source.getAlbumsCount().doubleValue()),
+                        Map.entry("Notes count", source.getNotesCount().doubleValue()),
+                        Map.entry("Pages count", source.getPagesCount().doubleValue()))))
                 .build();
     }
 }
