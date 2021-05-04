8d71b8eeb3c79bfdc6e9dec8688fedd6493ff481
Grig Alex <alexengrigdev@gmail.com>
2021-05-04 19:08:13 +0300

    Complete VkUserConverterTest#should_convert

--- src/test/java/dev/alexengrig/socnetgraphanalysis/converter/VkUserConverterTest.java
+++ src/test/java/dev/alexengrig/socnetgraphanalysis/converter/VkUserConverterTest.java
@@ -12,8 +12,6 @@ import com.vk.api.sdk.objects.users.responses.GetResponse;
 import dev.alexengrig.socnetgraphanalysis.domain.VkUser;
 import org.junit.jupiter.api.Test;
 
-import java.util.function.Function;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -30,48 +28,69 @@ class VkUserConverterTest {
         source.setBdate("1.1.2001");
         source.setSex(Sex.FEMALE);
         City city = new City();
-        city.setId(2);
+        {
+            city.setId(2);
+        }
         source.setCity(city);
         Country country = new Country();
-        country.setId(3);
+        {
+            country.setId(3);
+        }
         source.setCountry(country);
         source.setFriendStatus(FriendStatusStatus.OUTCOMING_REQUEST);
         source.setRelation(UserRelation.SINGLE);
         Personal personal = new Personal();
-        personal.setPolitical(4);
-        personal.setReligionId(5);
-        personal.setLifeMain(6);
-        personal.setPeopleMain(7);
-        personal.setSmoking(8);
-        personal.setAlcohol(9);
+        {
+            personal.setPolitical(4);
+            personal.setReligionId(5);
+            personal.setLifeMain(6);
+            personal.setPeopleMain(7);
+            personal.setSmoking(8);
+            personal.setAlcohol(9);
+        }
         source.setPersonal(personal);
         source.setCommonCount(10);
         UserCounters counters = new UserCounters();
-        counters.setFriends(11);
-        counters.setFollowers(12);
-        counters.setGroups(13);
-        counters.setAudios(14);
-        counters.setVideos(15);
-        counters.setPhotos(16);
-        counters.setAlbums(17);
-        counters.setNotes(18);
-        counters.setPages(19);
+        {
+            counters.setFriends(11);
+            counters.setFollowers(12);
+            counters.setGroups(13);
+            counters.setAudios(14);
+            counters.setVideos(15);
+            counters.setPhotos(16);
+            counters.setAlbums(17);
+            counters.setNotes(18);
+            counters.setPages(19);
+        }
         source.setCounters(counters);
 
         VkUser user = CONVERTER.convert(source);
 
         assertNotNull(user, "User is null");
-        assertUserEquals(source, GetResponse::getId, user, VkUser::getId, "Id");
-        assertUserEquals(source, GetResponse::getFirstName, user, VkUser::getFirstName, "First name");
-        assertUserEquals(source, GetResponse::getLastName, user, VkUser::getLastName, "Last name");
-        assertUserEquals(source, GetResponse::getBdate, user, VkUser::getBirthday, "Birthday");
-
+        assertEquals(source.getId(), user.getId(), "Id is incorrect");
+        assertEquals(source.getFirstName(), user.getFirstName(), "First name is incorrect");
+        assertEquals(source.getLastName(), user.getLastName(), "Last name is incorrect");
+        assertEquals(source.getBdate(), user.getBirthday(), "Birthday is incorrect");
+        assertEquals(source.getSex().ordinal(), user.getSex(), "Birthday is incorrect");
+        assertEquals(source.getCity().getId(), user.getCity(), "City is incorrect");
+        assertEquals(source.getCountry().getId(), user.getCountry(), "Country is incorrect");
+        assertEquals(source.getFriendStatus().ordinal(), user.getFriendStatus(), "FriendStatus is incorrect");
+        assertEquals(source.getRelation().ordinal(), user.getRelation(), "Relation is incorrect");
+        assertEquals(source.getPersonal().getPolitical(), user.getPolitical(), "Political is incorrect");
+        assertEquals(source.getPersonal().getReligionId(), user.getReligion(), "ReligionId is incorrect");
+        assertEquals(source.getPersonal().getLifeMain(), user.getLifeMain(), "LifeMain is incorrect");
+        assertEquals(source.getPersonal().getPeopleMain(), user.getPeopleMain(), "PeopleMain is incorrect");
+        assertEquals(source.getPersonal().getSmoking(), user.getSmoking(), "Smoking is incorrect");
+        assertEquals(source.getPersonal().getAlcohol(), user.getAlcohol(), "Alcohol is incorrect");
+        assertEquals(source.getCommonCount(), user.getCommonFriendsCount(), "CommonFriendsCount is incorrect");
+        assertEquals(source.getCounters().getFriends(), user.getFriendsCount(), "FriendsCount is incorrect");
+        assertEquals(source.getCounters().getFollowers(), user.getFollowersCount(), "FollowersCount is incorrect");
+        assertEquals(source.getCounters().getGroups(), user.getGroupsCount(), "GroupsCount is incorrect");
+        assertEquals(source.getCounters().getAudios(), user.getAudiosCount(), "AudiosCount is incorrect");
+        assertEquals(source.getCounters().getVideos(), user.getVideosCount(), "VideosCount is incorrect");
+        assertEquals(source.getCounters().getPhotos(), user.getPhotosCount(), "PhotosCount is incorrect");
+        assertEquals(source.getCounters().getAlbums(), user.getAlbumsCount(), "AlbumsCount is incorrect");
+        assertEquals(source.getCounters().getNotes(), user.getNotesCount(), "NotesCount is incorrect");
+        assertEquals(source.getCounters().getPages(), user.getPagesCount(), "PagesCount is incorrect");
     }
-
-    public <E, A, V> void assertUserEquals(E expected, Function<E, V> expectedGetter, A actual, Function<A, V> actualGetter, String name) {
-        V expectedValue = expectedGetter.apply(expected);
-        V actualValue = actualGetter.apply(actual);
-        assertEquals(expectedValue, actualValue, name + " is incorrect");
-    }
-
 }
