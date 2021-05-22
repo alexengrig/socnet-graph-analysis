8fb0a71bbb74718345c70fe8dab34972aad1a992
Grig Alex <alexengrigdev@gmail.com>
2021-05-22 19:57:00 +0300

    Update PageService

--- src/main/java/dev/alexengrig/socnetgraphanalysis/service/PageService.java
+++ src/main/java/dev/alexengrig/socnetgraphanalysis/service/PageService.java
@@ -1,7 +1,12 @@
 package dev.alexengrig.socnetgraphanalysis.service;
 
+import dev.alexengrig.socnetgraphanalysis.converter.ClusteringConditionModel2ClusteringRequestConverter;
+import dev.alexengrig.socnetgraphanalysis.converter.ClusteringResponse2ClusteringModelConverter;
 import dev.alexengrig.socnetgraphanalysis.converter.VkUserProperty2OptionConverter;
+import dev.alexengrig.socnetgraphanalysis.domain.ClusteringRequest;
+import dev.alexengrig.socnetgraphanalysis.domain.ClusteringResponse;
 import dev.alexengrig.socnetgraphanalysis.domain.VkUserProperty;
+import dev.alexengrig.socnetgraphanalysis.model.ClusteringConditionModel;
 import dev.alexengrig.socnetgraphanalysis.model.Option;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,10 +20,19 @@ import java.util.stream.Collectors;
 public class PageService {
 
     private final VkUserProperty2OptionConverter vkUserProperty2OptionConverter;
+    private final ClusteringService clusteringService;
+    private final ClusteringConditionModel2ClusteringRequestConverter clusteringConditionModel2ClusteringRequestConverter;
+    private final ClusteringResponse2ClusteringModelConverter clusteringResponse2ClusteringModelConverter;
 
     public List<Option> getPropertyOptions() {
         return Arrays.stream(VkUserProperty.values())
                 .map(vkUserProperty2OptionConverter::convert)
                 .collect(Collectors.toList());
+    }
+
+    public Object clustering(ClusteringConditionModel conditionModel) {
+        ClusteringRequest clusteringRequest = clusteringConditionModel2ClusteringRequestConverter.convert(conditionModel);
+        ClusteringResponse clusteringResponse = clusteringService.kMeans(clusteringRequest);
+        return clusteringResponse2ClusteringModelConverter.convert(clusteringResponse);
     }
 }
