7f46cc22b879ea3738f809ec37cb18f6ba6be21b
Grig Alex <alexengrigdev@gmail.com>
2021-05-22 20:35:06 +0300

    Update converters

--- src/main/java/dev/alexengrig/socnetgraphanalysis/converter/ClusterRecordConverter.java
+++ src/main/java/dev/alexengrig/socnetgraphanalysis/converter/ClusterRecordConverter.java
@@ -3,45 +3,32 @@ package dev.alexengrig.socnetgraphanalysis.converter;
 import dev.alexengrig.socnetgraphanalysis.domain.ClusterRecord;
 import dev.alexengrig.socnetgraphanalysis.domain.ClusterRecordParameters;
 import dev.alexengrig.socnetgraphanalysis.domain.VkUser;
-import org.springframework.core.convert.converter.Converter;
+import dev.alexengrig.socnetgraphanalysis.domain.VkUserProperty;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
-public class ClusterRecordConverter implements Converter<VkUser, ClusterRecord> {
+public class ClusterRecordConverter {
 
     @NonNull
-    @Override
-    public ClusterRecord convert(@NonNull VkUser source) {
+    public ClusterRecord convert(@NonNull VkUser source, Set<VkUserProperty> properties) {
         return ClusterRecord.builder()
                 .id(source.getId())
-                .label(String.join(" ", source.getFirstName(), source.getLastName()))
-                .parameters(new ClusterRecordParameters(Map.ofEntries(
-                        Map.entry("Accessed", source.isAccessed() ? 1d : 0d),
-                        Map.entry("Age", source.getAge().doubleValue()),
-                        Map.entry("Sex", source.getSex().doubleValue()),
-                        Map.entry("City", source.getCity().doubleValue()),
-                        Map.entry("Country", source.getCountry().doubleValue()),
-                        Map.entry("Friend status", source.getFriendStatus().doubleValue()),
-                        Map.entry("Relation", source.getRelation().doubleValue()),
-                        Map.entry("Political", source.getPolitical().doubleValue()),
-                        Map.entry("Religion", source.getReligion().doubleValue()),
-                        Map.entry("Life main", source.getLifeMain().doubleValue()),
-                        Map.entry("People main", source.getPeopleMain().doubleValue()),
-                        Map.entry("Smoking", source.getSmoking().doubleValue()),
-                        Map.entry("Alcohol", source.getAlcohol().doubleValue()),
-                        Map.entry("Common friends count", source.getCommonFriendsCount().doubleValue()),
-                        Map.entry("Friends count", source.getFriendsCount().doubleValue()),
-                        Map.entry("Followers count", source.getFollowersCount().doubleValue()),
-                        Map.entry("Audios count", source.getAudiosCount().doubleValue()),
-                        Map.entry("Videos count", source.getVideosCount().doubleValue()),
-                        Map.entry("Photos count", source.getPhotosCount().doubleValue()),
-                        Map.entry("Groups count", source.getGroupsCount().doubleValue()),
-                        Map.entry("Albums count", source.getAlbumsCount().doubleValue()),
-                        Map.entry("Notes count", source.getNotesCount().doubleValue()),
-                        Map.entry("Pages count", source.getPagesCount().doubleValue()))))
+                .label(getLabel(source))
+                .parameters(getParameters(source, properties))
                 .build();
+    }
+
+    private String getLabel(VkUser source) {
+        return String.join(" ", source.getFirstName(), source.getLastName());
+    }
+
+    private ClusterRecordParameters getParameters(VkUser source, Set<VkUserProperty> properties) {
+        Map<String, Double> map = properties.stream().collect(Collectors.toMap(VkUserProperty::getLabel, p -> p.get(source)));
+        return new ClusterRecordParameters(map);
     }
 }
--- src/main/java/dev/alexengrig/socnetgraphanalysis/converter/VkUserProperty2OptionConverter.java
+++ src/main/java/dev/alexengrig/socnetgraphanalysis/converter/VkUserProperty2OptionConverter.java
@@ -3,11 +3,13 @@ package dev.alexengrig.socnetgraphanalysis.converter;
 import dev.alexengrig.socnetgraphanalysis.domain.VkUserProperty;
 import dev.alexengrig.socnetgraphanalysis.model.Option;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 @Component
 public class VkUserProperty2OptionConverter implements Converter<VkUserProperty, Option> {
 
+    @NonNull
     @Override
     public Option convert(VkUserProperty source) {
         return Option.builder()
