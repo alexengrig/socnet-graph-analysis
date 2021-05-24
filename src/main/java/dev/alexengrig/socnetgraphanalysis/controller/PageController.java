08eff17f51b63b1993358cdbc27317dd6aafcb3a
Grig Alex <alexengrigdev@gmail.com>
2021-05-24 18:57:32 +0300

    Update PageController

--- src/main/java/dev/alexengrig/socnetgraphanalysis/controller/PageController.java
+++ src/main/java/dev/alexengrig/socnetgraphanalysis/controller/PageController.java
@@ -8,22 +8,35 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static java.util.Objects.isNull;
 
 @Controller
 @RequiredArgsConstructor
 public class PageController {
 
+    private final String vkOAuthUrl;
     private final PageService pageService;
 
     @GetMapping
-    public String index(Model model) {
+    public String index(@RequestParam(value = "code", required = false) String code, Model model) {
+        if (isNull(code)) {
+            return "redirect:" + vkOAuthUrl;
+        }
+        model.addAttribute("code", code);
         model.addAttribute("propertyOptions", pageService.getPropertyOptions());
         return "index";
     }
 
     @PostMapping("/clustering")
-    public String clustering(@ModelAttribute("condition") ClusteringConditionModel condition, Model model) {
-        model.addAttribute("clustering", pageService.clustering(condition));
+    public String clustering(@RequestParam(value = "code", required = false) String code,
+                             @ModelAttribute("condition") ClusteringConditionModel condition,
+                             Model model) {
+        if (isNull(code)) {
+            return "redirect:" + vkOAuthUrl;
+        }
+        model.addAttribute("clustering", pageService.clustering(code, condition));
         return "clustering";
     }
 }
