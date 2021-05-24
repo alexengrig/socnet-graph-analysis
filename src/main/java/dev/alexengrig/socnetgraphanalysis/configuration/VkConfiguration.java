91f295488ac111fafea703d0531b3305385b5a71
Grig Alex <alexengrigdev@gmail.com>
2021-05-24 18:59:39 +0300

    Update VkConfiguration

--- src/main/java/dev/alexengrig/socnetgraphanalysis/configuration/VkConfiguration.java
+++ src/main/java/dev/alexengrig/socnetgraphanalysis/configuration/VkConfiguration.java
@@ -2,16 +2,23 @@ package dev.alexengrig.socnetgraphanalysis.configuration;
 
 import com.vk.api.sdk.client.TransportClient;
 import com.vk.api.sdk.client.VkApiClient;
-import com.vk.api.sdk.client.actors.ServiceActor;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
-import com.vk.api.sdk.queries.friends.FriendsGetQuery;
-import com.vk.api.sdk.queries.users.UsersGetQuery;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class VkConfiguration {
+
+    @Bean
+    public String vkOAuthUrl(@Value("${application.vk.app-id}") String clientId,
+                             @Value("${application.vk.oauth.redirect-url}") String redirectUrl,
+                             @Value("${application.vk.oauth.version}") String version) {
+        return "https://oauth.vk.com/authorize?display=page&response_type=code" +
+                "&redirect_uri=" + redirectUrl +
+                "&v=" + version +
+                "&client_id=" + clientId;
+    }
 
     @Bean
     public TransportClient transportClient() {
@@ -21,23 +28,5 @@ public class VkConfiguration {
     @Bean
     public VkApiClient vkApiClient(TransportClient transportClient) {
         return new VkApiClient(transportClient);
-    }
-
-    @Bean
-    public ServiceActor serviceActor(@Value("${application.vk.app-id}") String appId,
-                                     @Value("${application.vk.access-token}") String accessToken) {
-        return new ServiceActor(Integer.valueOf(appId), accessToken);
-    }
-
-    /* Queries */
-
-    @Bean
-    public UsersGetQuery usersGetQuery(VkApiClient client, ServiceActor actor) {
-        return new UsersGetQuery(client, actor);
-    }
-
-    @Bean
-    public FriendsGetQuery friendsGetQuery(VkApiClient client, ServiceActor actor) {
-        return new FriendsGetQuery(client, actor);
     }
 }
