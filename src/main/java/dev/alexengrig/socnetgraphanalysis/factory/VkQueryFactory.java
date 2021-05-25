408662eb72d09475138fa5181c7f1b86db6d690d
Grig Alex <alexengrigdev@gmail.com>
2021-05-26 00:02:46 +0300

    Update VkQueryFactory

--- src/main/java/dev/alexengrig/socnetgraphanalysis/factory/VkQueryFactory.java
+++ src/main/java/dev/alexengrig/socnetgraphanalysis/factory/VkQueryFactory.java
@@ -2,53 +2,33 @@ package dev.alexengrig.socnetgraphanalysis.factory;
 
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.UserActor;
-import com.vk.api.sdk.exceptions.ApiException;
-import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.objects.UserAuthResponse;
 import com.vk.api.sdk.queries.friends.FriendsGetQuery;
 import com.vk.api.sdk.queries.users.UsersGetQuery;
-import dev.alexengrig.socnetgraphanalysis.exception.VkSdkException;
-import org.springframework.beans.factory.annotation.Value;
+import dev.alexengrig.socnetgraphanalysis.service.VkAuthorizationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class VkQueryFactory {
 
     private final VkApiClient client;
-    private final Integer appId;
-    private final String clientSecret;
-    private final String redirectUrl;
-
-    public VkQueryFactory(VkApiClient client,
-                          @Value("${application.vk.app-id}") Integer appId,
-                          @Value("${application.vk.access-token}") String clientSecret,
-                          @Value("${application.vk.oauth.redirect-url}") String redirectUrl) {
-        this.client = client;
-        this.appId = appId;
-        this.clientSecret = clientSecret;
-        this.redirectUrl = redirectUrl;
-    }
-
-    public UserAuthResponse auth(String code) {
-        try {
-            return client.oAuth()
-                    .userAuthorizationCodeFlow(appId, clientSecret, redirectUrl, code)
-                    .execute();
-        } catch (ApiException | ClientException e) {
-            throw new VkSdkException("Exception of authentication", e);
-        }
-    }
+    private final VkAuthorizationService authorization;
 
     public UserActor userActor(String code) {
-        UserAuthResponse auth = auth(code);
+        UserAuthResponse auth = authorization.auth(code);
         return new UserActor(auth.getUserId(), auth.getAccessToken());
     }
 
+    @Cacheable("vkUserGetQueries")
     public UsersGetQuery usersGetQuery(String code) {
         UserActor actor = userActor(code);
         return new UsersGetQuery(client, actor);
     }
 
+    @Cacheable("vkFriendsGetQueries")
     public FriendsGetQuery friendsGetQuery(String code) {
         UserActor actor = userActor(code);
         return new FriendsGetQuery(client, actor);
